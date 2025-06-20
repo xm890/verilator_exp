449febfecea4cfb93b171c51a8b59d173a6e08b9
Liu Hanwen <lhwen1998@std.uestc.edu.cn>
2025-06-20 08:04:41 +0800

    verilator exp2

--- tb_alu.cpp
+++ tb_alu.cpp
@@ -4,23 +4,80 @@
 #include <verilated_vcd_c.h>
 #include "Valu.h"
 #include "Valu___024unit.h"
-
-#define MAX_SIM_TIME 20
+#include <cstdlib>
+#define MAX_SIM_TIME 200
+#define VERIF_START_TIME 7
 vluint64_t sim_time = 0;
+vluint64_t posedge_cnt =0;
 
+
+void set_rnd_out_valid(Valu *dut, vluint64_t &sim_time){
+    if (sim_time >= VERIF_START_TIME) {
+        dut->in_valid = rand() % 2; // generate values 0 and 1
+    }
+}
+
+
+
+void check_out_valid(Valu *dut, vluint64_t &sim_time){
+	static unsigned char in_valid =0;
+	static unsigned char in_valid_d =0;
+	static unsigned char out_valid_exp =0;
+
+	if(sim_time >= VERIF_START_TIME){
+			out_valid_exp = in_valid_d;
+			in_valid_d = in_valid;
+			in_valid = dut->in_valid;
+			if(out_valid_exp != dut->out_valid){
+					  std::cout << "ERROR: out_valid mismatch, "
+                << "exp: " << (int)(out_valid_exp)
+                << " recv: " << (int)(dut->out_valid)
+                << " simtime: " << sim_time << std::endl;
+
+				}
+
+		}
+	}
+
+
+
+void dut_reset(Valu *dut, vluint64_t &sim_time){
+				
+		dut->rst = 0;
+		if(sim_time >= 3 && sim_time <6){
+				dut->rst = 1;
+      	dut->a_in = 0;
+      	dut->b_in = 0;
+      	dut->op_in = 0;
+			}
+
+	}
 int main(int argc, char** argv, char** env) {
+		srand(time(NULL));
+		Verilated::commandArgs(argc,argv);
     Valu *dut = new Valu;
 
     Verilated::traceEverOn(true);
     VerilatedVcdC *m_trace = new VerilatedVcdC;
     dut->trace(m_trace, 5);
     m_trace->open("waveform.vcd");
-
     while (sim_time < MAX_SIM_TIME) {
-        dut->clk ^= 1;
-        dut->eval();
-        m_trace->dump(sim_time);
-        sim_time++;
+				dut_reset(dut,sim_time);	
+				dut->clk ^=1;
+				dut->eval();
+				if(dut->clk == 1){
+						dut->in_valid = 0;
+						posedge_cnt ++;
+						set_rnd_out_valid(dut,sim_time);
+						check_out_valid(dut,sim_time);
+						if(posedge_cnt == 7){
+								if(dut->out_valid !=1)
+									std::cout << "ERROR" << std::endl;
+							}
+						}
+					
+       	m_trace->dump(sim_time);
+       	sim_time++;
     }
 
     m_trace->close();
